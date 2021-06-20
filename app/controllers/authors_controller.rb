21bf38fcdfbdfbf8968e3625dcb556aa021af95c
Divyanshu Sharma <divyanshu.sharma1401@gmail.com>
2021-06-20 18:43:23 +0530

    Remove extra lines

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,26 +1,21 @@
 class AuthorsController < ApplicationController
     before_action :set_author, only: %i[ show edit update destroy ]
-  
-    # GET /authors or /authors.json
+
     def index
       @authors = Author.all
     end
   
-    # GET /authors/1 or /authors/1.json
     def show
     end
   
-    # GET /authors/new
     def new
       @author = Author.new
       # @books= Book.all.pluck(:book_title)
     end
   
-    # GET /authors/1/edit
     def edit
     end
   
-    # POST /authors or /authors.json
     def create
       @author = Author.new(author_params)
       unless author_params[:date_of_birth].empty?
@@ -33,8 +28,7 @@ class AuthorsController < ApplicationController
           render :new
         end
     end
-  
-    # PATCH/PUT /authors/1 or /authors/1.json
+
     def update
         if @author.update(author_params)
           redirect_to @author
@@ -43,7 +37,6 @@ class AuthorsController < ApplicationController
         end
     end
   
-    # DELETE /authors/1 or /authors/1.json
     def destroy
       @author.destroy
       redirect_to authors_path
@@ -51,12 +44,11 @@ class AuthorsController < ApplicationController
     end
   
     private
-      # Use callbacks to share common setup or constraints between actions.
+
       def set_author
         @author = Author.find(params[:id])
       end
   
-      # Only allow a list of trusted parameters through.
       def author_params
         params.require(:author).permit(:author_id, :first_name, :last_name, :date_of_birth)
       end
